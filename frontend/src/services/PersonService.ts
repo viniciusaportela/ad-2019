e377e670cf1f0b6d66e46c245d05f8f3bb8589c7
viniciusaportela <vinicius.portela.stm@gmail.com>
2020-09-15 22:04:51 -0300

    Implement PersonService list, create, update, delete and sendToAll methods

--- frontend/src/services/PersonService.ts
+++ frontend/src/services/PersonService.ts
@@ -1,27 +1,32 @@
+import axios from "axios";
+import { API_SERVER } from "../constants";
+
 export default class PersonService {
   static async list() {
-    try {
-      throw "a";
-
-      return [{ id: "asd", name: "a", email: "a" }];
-    } catch (e) {
-      throw e;
-    }
+    const res = await axios.get<Person[]>(`${API_SERVER}/v1/people/`);
+    return res.data;
   }
 
   static async create(name: string, email: string) {
-    try {
-      throw "a";
-
-      return { id: "asd", name: "a", email: "a" };
-    } catch (e) {
-      throw e;
-    }
+    const res = await axios.post<Person>(`${API_SERVER}/v1/people/`, {
+      name,
+      email,
+    });
+    return res.data;
   }
 
-  static async update() {}
+  static async update(personId: string, name: string, email: string) {
+    await axios.put<Person>(`${API_SERVER}/v1/people/${personId}`, {
+      name,
+      email,
+    });
+  }
 
-  static async delete() {}
+  static async delete(personId: string) {
+    await axios.delete(`${API_SERVER}/v1/people/${personId}`);
+  }
 
-  static async sendToAll() {}
+  static async sendToAll() {
+    await axios.post(`${API_SERVER}/v1/people/send-to-all`);
+  }
 }
