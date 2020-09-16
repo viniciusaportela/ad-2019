1a0a89c30e061f79c910a1db9321b20cf37f9503
viniciusaportela <vinicius.portela.stm@gmail.com>
2020-09-16 11:59:55 -0300

    Add build folder

---
+++ frontend/build/precache-manifest.456220994c834aa8252f31244bdd0336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534aa28d36bd225988abb79e58009ca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05bd40be3b2b974ec952",
+    "url": "/static/css/main.a18ef55b.chunk.css"
+  },
+  {
+    "revision": "38097f5c066126499cae",
+    "url": "/static/js/2.b04622b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b04622b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bd40be3b2b974ec952",
+    "url": "/static/js/main.a4bef0cd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
